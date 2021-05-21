d6b535e40bdc68f20559b2dca559d19b491e3c64
SirdsH <svatopluk.houserek@educanet.cz>
2021-05-21 12:45:43 +0200

    commit

--- src/test/java/cz/educanet/OperationsTest.java
+++ src/test/java/cz/educanet/OperationsTest.java
@@ -11,12 +11,18 @@ class OperationsTest {
     void getPlus() {
         int result = op.getPlus(2, 2);
         assertEquals(4, result);
+
+        int result1 = op.getPlus(5, 2);
+        assertEquals(7, result1);
     }
 
     @Test
     void getMinus() {
         int result = op.getMinus(10, 10);
         assertEquals(0, result);
+
+        int result1 = op.getMinus(10, 3);
+        assertEquals(7, result1);
     }
 
     @Test
@@ -29,17 +35,26 @@ class OperationsTest {
     void getDivide() {
         double result = op.getDivide(50, 5);
         assertEquals(10, result);
+
+        double result1 = op.getDivide(60, 20);
+        assertEquals(3, result1);
     }
 
     @Test
     void getPow() {
         double result = op.getPow(5, 2);
         assertEquals(25, result);
+
+        double result1 = op.getPow(10, 2);
+        assertEquals(100, result1);
     }
 
     @Test
     void getFactorial() {
         int result = op.getFactorial(5);
         assertEquals(120, result);
+
+        int result1 = op.getFactorial(7);
+        assertEquals(5040, result1);
     }
 }
